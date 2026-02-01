f02aadac28bbc420af69c9adda23f2d71664dcfa
Bartroz <rozwadowki512@gmail.com>
2026-02-01 22:22:17 +0100

    Refactored code structure and started working on main.py file

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from sql_conn import checkIfTableExist, createTables
+from sql_conn import add_word_to_main_db
+from google_sheet_con import check_if_google_sheet_updated, download_from_database, split_data_to_rows
 
-if not checkIfTableExist():
-    createTables()
+
+def operationsOnDB():
+    googleSheetLength = download_from_database()
+    listOfWords = split_data_to_rows()
+    dbLength = check_if_google_sheet_updated()
+    if dbLength == 0 or dbLength is None:
+        add_word_to_main_db(listOfWords)
+
+if __name__ == "__main__":
+    operationsOnDB()
--- sql_conn.py
+++ sql_conn.py
@@ -1,8 +1,76 @@
 import sqlite3
 
-not_exist:bool = True
+def check_if_table_exist() -> None: #sprawdzanie czy DB istnienią - sprawdzane przy uruchomieniu
 
-def checkIfWordExist(wordToSearch:str,meaning:str) -> None: #sprawdzanie czy słowo istnieje w DB
+    tableNames:list[str] = ["WordsFromGoogleSheet","LearnedWords", "WordsToPractice","DbDataInfo" ]
+    existingTablesDb:list[str] = []
+    notExistingTablesDb:list[str] = []
+    score:int = 0
+
+    try:
+        with sqlite3.connect("GermanLearning.db") as connection:
+            cursor = connection.cursor()
+            for name in tableNames:
+
+                sqlquery = """
+                SELECT name 
+                FROM sqlite_master
+                WHERE type = 'table'
+                AND name = ?
+                """
+                cursor.execute(sqlquery,(name,))
+                existingTablesDb.append(cursor.fetchone())
+
+            for i,exist in enumerate(existingTablesDb):
+
+                if exist is not None:
+                    if exist[0] == tableNames[i]:
+                        score += 1
+                elif exist is None:
+                    notExistingTablesDb.append(tableNames[i])
+                    score += 0
+
+            if score != len(tableNames):
+                return False,notExistingTablesDb
+            else:    
+                return True,[]    
+
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+
+def create_tables() -> None: #tworzenie tabeli
+
+    query1 = """
+    CREATE Table {tableName} (
+    id INTEGER PRIMARY KEY ,
+    word TEXT,
+    meaning TEXT
+    ) 
+    """
+
+    query2 = """
+    CREATE Table {tableName} (
+    id INTEGER PRIMARY KEY,
+    length INT
+    )
+    """
+
+    with sqlite3.connect("GermanLearning.db") as connection:
+        try:
+            cursor = connection.cursor()
+            
+            for name in missingTables:
+                
+                if name == "DbDataInfo":
+                    cursor.execute(query2.format(tableName = name))
+                else:
+                    cursor.execute(query1.format(tableName = name))
+
+            not_exist = True
+        except sqlite3.Error as e:
+            print(f"Error: {e}")
+            
+def check_if_word_exist(wordToSearch:str,meaning:str) -> None: #sprawdzanie czy słowo istnieje w DB
     global cursor
     try:
         with sqlite3.connect('GermanLearning.db') as connection:
@@ -15,84 +83,35 @@ def checkIfWordExist(wordToSearch:str,meaning:str) -> None: #sprawdzanie czy sł
             check = cursor.fetchone()
 
             if check is None:
-                insertWordToDB(wordToSearch,meaning)
-            else:
-                pass
+                insert_word_to_DB(wordToSearch,meaning)
                 
     except sqlite3.Error as e:
         print(f"Error occured: {e}")
 
-def insertWordToDB(wordToAdd:str,meaningToAdd:str) -> None:
+def insert_word_to_DB(wordToAdd:str,meaningToAdd:str) -> None:
     global cursor
     cursor.execute("INSERT INTO WordsToPractice (word,meaning) VALUES (?,?)",
     (wordToAdd,meaningToAdd,))
     print("Dodano słowo")
 
-def checkIfTableExist(): #sprawdzanie czy DB istnienią - sprawdzane przy uruchomieniu
-
-    tableNames:list[str] = ["WordsFromGoogleSheet","LearnedWords", "WordsToPractice", "DbDataInfo" ]
-    existingTablesDb:list[str] = []
-    notExistingTablesDb:list[str] = []
-    score:int = 0
-
-    if (not_exist):
-        try:
-            with sqlite3.connect("GermanLearning.db") as connection:
-                cursor = connection.cursor()
-                for name in tableNames:
-
-                    sqlquery = """
-                    SELECT name 
-                    FROM sqlite_master
-                    WHERE type = 'table'
-                    AND name = ?
-                    """
-                    cursor.execute(sqlquery,(name,))
-                    existingTablesDb.append(cursor.fetchone())
-
-                for i,exist in enumerate(existingTablesDb):
-
-                    if exist is not None:
-                        if exist[0] == tableNames[i]:
-                            score += 1
-                    elif exist is None:
-                        notExistingTablesDb.append(tableNames[i])
-                        score += 0
-
-                if score != len(tableNames):
-                    return False,notExistingTablesDb
-                else:    
-                    return True,[]    
-
-        except sqlite3.Error as e:
-            print(f"Error: {e}")
-
-
-def createTables(tablesToAdd:list[str]): #tworzenie tabeli
-
-    query = """
-    CREATE Table {table_name} (
-    id INTEGER PRIMARY KEY ,
-    word TEXT,
-    meaning TEXT
-    ) 
-    """
-
+def check_if_google_sheet_updated():
     with sqlite3.connect("GermanLearning.db") as connection:
         try:
             cursor = connection.cursor()
-            
-            for name in tablesToAdd:
-                
-                cursor.execute(query.format(table_name = name))
+            sqlQuery = """
+            SELECT COUNT(*) FROM WordsFromGoogleSheet
+            """
 
-            not_exist = True
+            cursor.execute(sqlQuery)
+            dbLength:int = cursor.fetchone()
+
+            return dbLength[0]
         except sqlite3.Error as e:
-            print(f"Error: {e}")
-            
+            print("Error occured : {e}")
+
+
 
 if __name__ == "__main__":
-    (status,missingTables) = checkIfTableExist()
+    (status,missingTables) = check_if_table_exist()
     if not status:
-        createTables(missingTables)
-
+        create_tables()
